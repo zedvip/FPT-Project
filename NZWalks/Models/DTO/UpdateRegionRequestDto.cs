64e34add5f69ce965e70109636e6cde3a5dd01bd
zedvip <nhinhanh904@gmail.com>
2025-02-19 15:40:05 +0700

    delete projects

--- NZWalks/Models/DTO/UpdateRegionRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NZWalks.Models.DTO
-{
-    public class UpdateRegionRequestDto
-    {
-        public string Code { get; set; }
-        public string Name { get; set; }
-        public string? RegionImageUrl { get; set; }
-    }
-}
