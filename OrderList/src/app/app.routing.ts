44d39ab7cabbed6d09de6b254a64ac5ba0efa261
zedvip <nhinhanh904@gmail.com>
2025-02-19 12:05:04 +0700

    commit

--- OrderList/src/app/app.routing.ts
+++ OrderList/src/app/app.routing.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { OrderListComponent } from './order-list/order-list.component';
+import { OrderListComponent } from './admin/order-list/order-list.component';
 import { AddOrderComponent } from './order-add/order-add.component';
+
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: 'order-list', pathMatch: 'full' },
-  { path: 'order-list', component: OrderListComponent },
+   { path: 'order-list', component: OrderListComponent },
   { path: 'add-order', component: AddOrderComponent },
 ];
